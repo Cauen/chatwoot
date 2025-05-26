d9b2fe8d31c4b9964aa6ace3fdbd3f76945dc30e
cauen <emanuel-caue@hotmail.com>
2025-05-26 08:17:13 -0300

    Agente

--- app/services/whatsapp/providers/whatsapp_cloud_service.rb
+++ app/services/whatsapp/providers/whatsapp_cloud_service.rb
@@ -82,7 +82,7 @@ class Whatsapp::Providers::WhatsappCloudService < Whatsapp::Providers::BaseServi
         messaging_product: 'whatsapp',
         context: whatsapp_reply_context(message),
         to: phone_number,
-        text: { body: message.content },
+        text: { body: format_message_content(message) },
         type: 'text'
       }.to_json
     )
@@ -96,7 +96,7 @@ class Whatsapp::Providers::WhatsappCloudService < Whatsapp::Providers::BaseServi
     type_content = {
       'link': attachment.download_url
     }
-    type_content['caption'] = message.content unless %w[audio sticker].include?(type)
+    type_content['caption'] = format_message_content(message) unless %w[audio sticker].include?(type)
     type_content['filename'] = attachment.file.filename if type == 'document'
     response = HTTParty.post(
       "#{phone_id_path}/messages",
@@ -156,5 +156,23 @@ class Whatsapp::Providers::WhatsappCloudService < Whatsapp::Providers::BaseServi
     )
 
     process_response(response)
+  end
+
+  private
+
+  def format_message_content(message)
+    return message.content unless should_add_agent_prefix?(message)
+
+    agent_name = message.sender&.available_name || message.sender&.name
+    return message.content unless agent_name
+
+    "**#{agent_name}:**\n#{message.content}"
+  end
+
+  def should_add_agent_prefix?(message)
+    return false unless message.sender.is_a?(User)
+    return false unless [4, 5].include?(message.conversation.account_id)
+
+    true
   end
 end
